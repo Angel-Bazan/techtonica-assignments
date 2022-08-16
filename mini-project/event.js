7fa2b7fdf0f3f2298cf60c8044f6bc37d15b3064
TPL522_14 <tpl522_14@TPL522-14.lan>
2022-08-15 19:20:07 -0700

    First commit index.html

---
+++ mini-project/event.js
@@ -0,0 +1,23 @@
+class Event {
+    constructor(name, description) {
+      this.name = name;
+      this.description = description;
+      this.availableTickets = [];
+    }
+  }
+  const eventObj1 = new Event(
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+  );
+
+const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019'); 
+
+const eventArray = new Array();
+
+eventArray.push(eventObj1);
+// pushing multiple objects to an array at once
+eventArray.push(eventObj1, eventObj2, eventObj3);
+
+console.log(eventArray);
+
