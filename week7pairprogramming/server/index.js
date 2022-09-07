9a394d06954281c61100e28734389e5afd95b7ae
Angel <angelsanchez.as524@gmail.com>
2022-09-07 15:17:23 -0700

    first commit

--- week7pairprogramming/server/index.js
+++ week7pairprogramming/server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'; 
 import cors from 'cors';
+import { config } from "dotenv"; config();
 
 const app = express(); 
 const PORT = 5000; 
@@ -7,17 +8,21 @@ const PORT = 5000;
 app.use(cors()); 
 
 
+
 app.get('/', (req, res) => {
     res.json("Hello from Techtonica")
 });
 
-app.get('/api/weather', (req, res) => {
+
     // Variable to store your API Key
-  const APIKEY = "91290aa34b77dd1517169822a284b6a8";
-
+  
   // create the correct URL to fetch
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
+  const url = "https://api.openweathermap.org/data/2.5/" 
+  app.get('/api/weather', (req,res)=> { 
+    
+    fetch(`${url}weather?q=london&APPID=${process.env.REACT_APP_API}&units=imperial`)  
+    .then((response) => response.json()) 
+    .then(data =>  res.send(data) );})
 
-    res.json(url);
-});
+
 app.listen(PORT, () => console.log(`Hola! server running on port http://localhost:${PORT}`));
